4e53fccbbe3b6c0563c186c4fa18ddbe1669443d
Arya Zhao <lz2650@columbia.edu>
2020-07-25 04:03:19 +0800

    install and test material UI, uninstall bootstrap

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,17 +3,13 @@ import React from 'react';
 
 export const Header = () => (
   <Head>
+    <meta
+      name="viewport"
+      content="minimum-scale=1, initial-scale=1, width=device-width"
+    />
     <title>Arya Lingyu Zhao</title>
     <link rel="icon" href="/favicon.ico" />
     <link href="https://fonts.googleapis.com/css?family=Crimson+Text&display=swap" rel="stylesheet" />
-    <script src="https://unpkg.com/react/umd/react.production.min.js" crossorigin></script>
-
-    <script
-      src="https://unpkg.com/react-dom/umd/react-dom.production.min.js"
-      crossorigin></script>
-
-    <script
-      src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js"
-      crossorigin></script>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
   </Head>
 );
--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -1,18 +1,10 @@
 import React from 'react'
-import {Navbar} from 'react-bootstrap'
-import Link from 'next/link'
+import Button from '@material-ui/core/Button';
 import styled from 'styled-components'
 
 
 export const Navigation = () => (
-  <Navbar bg="light" expand="lg">
-    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="mr-auto">
-        <Link><Nav.Link href="#home">Home</Nav.Link></Link>
-        <Link><Nav.Link href="#link">Link</Nav.Link></Link>
-      </Nav>
-    </Navbar.Collapse>
-  </Navbar>
+  <Button variant="contained" color="primary">
+      Hello World
+  </Button>
 );
