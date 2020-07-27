fa191fc5ac160802582ceae9ceca56854a40032f
Arya Zhao <lz2650@columbia.edu>
2020-07-28 06:29:11 +0800

    add hotjar

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,5 +1,18 @@
 import Head from 'next/head'
 
+const hotjar = () =>
+{__html: `
+(function(h,o,t,j,a,r){
+  h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+  h._hjSettings={hjid:1920486,hjsv:6};
+  a=o.getElementsByTagName('head')[0];
+  r=o.createElement('script');r.async=1;
+  r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+  a.appendChild(r);
+})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');"};`
+}
+
+
 export const Header = () => (
   <Head>
     <meta
@@ -8,8 +21,10 @@ export const Header = () => (
     />
     <title>Arya Lingyu Zhao</title>
     <link rel="icon" href="/columbia-university-logo.ico" />  
+    
+    <script>{hotjar}</script>
     {/* <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"></link> */}
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Crimson+Text&display=swap"  />
     <link rel="stylesheet" href = "https://fonts.googleapis.com/icon?family=Material+Icons" />
   </Head>
-);
+)
--- components/Internships.jsx
+++ components/Internships.jsx
@@ -7,7 +7,10 @@ const dtx = {
     title: "Software Engineer Intern (full-time)",
     location: "NY, NYC",
     time: "Jun. 2020 - Aug. 2020",
-    description: "- Developed, tested and deployed website converter microservice, and integrated new features into website builder Flowpage API, <b>shortening 98% of transition time</b> for users from company’s competitors<br/> - Coded in <b>Node.js</b> using <b>TypeScript, Express, Multer, Cheerio.js, Hapi.js, Axios, and REST</b>; managed <b>Azure SQL DB</b> and <b>Firebase</b> using <b>TypeORM, GraphQL, Apollo</b>; tested and deployed using <b>Docker, Jest, SuperTest, and ESlint</b> <br/>- Contributed around <b>14,000 lines of code</b> in the first month, and completed all tasks within half of deadline time<br/>- Collaborated in team of 11 developers and PM using Agile and GitHub in <b>12-week paid internship</b><br/>"
+    description: 
+    `- Developed, tested and deployed website converter microservice, and integrated new features into website builder Flowpage API, <b>shortening 98% of transition time</b> for users from company’s competitors<br/> 
+    - Coded in <b>Node.js</b> using <b>TypeScript, Express, Multer, Cheerio.js, Hapi.js, Axios, and REST</b>; managed <b>Azure SQL DB</b> and <b>Firebase</b> using <b>TypeORM, GraphQL, Apollo</b>; tested and deployed using <b>Docker, Jest, SuperTest, and ESlint</b> <br/>
+    - Contributed around <b>14,000 lines of code</b> in the first month, and completed all tasks within half of deadline time<br/>- Collaborated in team of 11 developers and PM using Agile and GitHub in <b>12-week paid internship</b><br/>`
 }
 
 const makeithappen = {
