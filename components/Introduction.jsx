fedba728f273ec05589c9088e159ca2ebae697d1
Arya Zhao <lz2650@columbia.edu>
2020-07-28 06:35:21 +0800

    add react repo

--- components/Introduction.jsx
+++ components/Introduction.jsx
@@ -37,6 +37,8 @@ export const Introduction = () => {
           <IconButton color="inherit" href="https://www.youtube.com/channel/UCM9HMBZjUXnoZej-xxFaCyQ" target="_blank"><YouTube /></IconButton>
         </Grid>
       </Grid>
-      <Typography variant="body1" align="center">This page is built with React, Next, JSX, and Mateiral UI</Typography>
+      <Typography variant="body1" align="center">This page is built with React, Next, JSX, and Mateiral UI. {" "}
+        <a href="https://github.com/AryaZhao/arya-lingyu-zhao.git">codes here</a>
+      </Typography>
     </Box>
 )}
