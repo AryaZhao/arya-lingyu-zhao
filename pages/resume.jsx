c5a6a386ceef9de7c878c1b58c0292ca6196e346
Arya Zhao <lz2650@columbia.edu>
2020-08-07 06:17:07 +0800

    add resume

---
+++ pages/resume.jsx
@@ -0,0 +1,14 @@
+import Header from '../components/Header';
+const Resume = () => (
+  <>
+    <Header />
+    <iframe
+      frameborder="0"
+      scrolling="no"
+      width="100%"
+      height="800px"
+      src="/AryaZhao_Resume.pdf"></iframe>
+  </>
+);
+
+export default Resume;
