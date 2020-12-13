e3978168478b2f9cb62c393fb136669ddaec6f37
Arya Zhao <lz2650@columbia.edu>
2020-12-13 13:09:40 +0800

    update work experience

--- components/Internships.jsx
+++ components/Internships.jsx
@@ -1,8 +1,20 @@
 import { Typography } from '@material-ui/core';
 import Internship from './Internship';
 
+const oracle = {
+  company: 'Oracle Corporation',
+  link: 'https://www.oracle.com/index.html',
+  title: 'Incoming Software Engineer (full-time)',
+  location: 'Redwood City, Bay Area',
+  time: 'Start in Jun. 2020',
+  description:
+    `- Developed, tested and deployed website converter microservice, and integrated new features into website builder Flowpage API, <b>shortening 98% of transition time</b> for users from company’s competitors<br/> 
+    - Coded in <b>Node.js</b> using <b>TypeScript, Express, Multer, Cheerio.js, Hapi.js, Axios, and REST</b>; managed <b>Azure SQL DB</b> and <b>Firebase</b> using <b>TypeORM, GraphQL, Apollo</b>; tested and deployed using <b>Docker, Jest, SuperTest, and ESlint</b> <br/>
+    - Contributed around <b>14,000 lines of code</b> in the first month, and completed all tasks within half of deadline time<br/>- Collaborated in team of 11 developers and PM using Agile and GitHub in <b>12-week paid internship</b><br/>`,
+};
+
 const dtx = {
-  company: 'the dtx company',
+  company: 'Flowcode',
   link: 'https://www.thedtxcompany.com/',
   title: 'Software Engineering Intern (full-time)',
   location: 'NY, NYC',
@@ -37,10 +49,11 @@ const Internships = () => (
   <div id="internships">
     <br />
     <Typography variant="h4">
-      Internships
+      Work Experience
     </Typography>
     <hr />
     <br />
+    <Internship info={oracle} />
     <Internship info={dtx} />
     <Internship info={makeithappen} />
     <Internship info={utofun} />
