ec7b49d0f94b8796e902d0b8b8f5ba03b623c908
Arya Zhao <lz2650@columbia.edu>
2021-04-11 13:20:29 -0700

    add flute video

--- components/Flute.jsx
+++ components/Flute.jsx
@@ -8,9 +8,12 @@ const Flute = () => (
     </Typography>
     <hr />
     <br />
+    
+    <iframe title="Bach Sonata in B Minor - Largo" width="560" height="315" src="https://www.youtube.com/embed/BmghjxD4iVc" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
+    <iframe title="Beethoven Grand Trio Op 87 - I" width="560" height="315" src="https://www.youtube.com/embed/ODTUjh1JFqU" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
     <iframe title="Spring is Comming" width="560" height="315" src="https://www.youtube.com/embed/lioERL2AphE" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
-
-    <iframe title="Beethoven Grand Trio Op 87 - I" width="560" height="315" src="https://www.youtube.com/embed/31Nr4Ejr9Oo" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
+    
+    
   </div>
 );
 
