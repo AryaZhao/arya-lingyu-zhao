50d9ddd46ba7a39868e384c10b8f88b8af07a5ae
Arya Zhao <lz2650@columbia.edu>
2021-04-27 20:45:10 -0700

    add flute videos

--- components/Flute.jsx
+++ components/Flute.jsx
@@ -10,10 +10,12 @@ const Flute = () => (
     <br />
     
     <iframe title="Bach Sonata in B Minor - Largo" width="560" height="315" src="https://www.youtube.com/embed/BmghjxD4iVc" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
+    <iframe title="Handel Sonata in C Major" width="560" height="315" src="https://www.youtube.com/embed/sQaZayYTl7E" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
     <iframe title="Beethoven Grand Trio Op 87 - I" width="560" height="315" src="https://www.youtube.com/embed/ODTUjh1JFqU" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
-    <iframe title="Spring is Comming" width="560" height="315" src="https://www.youtube.com/embed/lioERL2AphE" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
+    <iframe title="Spring is Comming" width="560" height="315" src="https://www.youtube.com/embed/R6ro4YSJfEw" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
+    <iframe title="Villa-Lobos Jet Whistle" width="560" height="315" src="https://www.youtube.com/embed/rlZZMPZK3OU" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
     
-    
+
   </div>
 );
 
